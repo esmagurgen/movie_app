cefa748d1c604099041a46e42c272b06c4e54a3b
esmagurgen <esmagrgn01@gmail.com>
2026-06-17 21:27:09 +0300

    initial commit

---
+++ constants/colors.ts
@@ -0,0 +1,11 @@
+const Colors = {
+  primary: "#2F78FF", 
+  background: "#000000",
+  text: "#FFFFFF",
+  gray: "#9EA0A2",
+  surface: "#131313",
+  border: "#1F1F1F",
+  darkBorder: "#202020"
+};
+
+export default Colors;
