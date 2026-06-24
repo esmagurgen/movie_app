de5a2d5a3b7028c80bcf87e99b73e4af25b25fda
esmagurgen <esmagrgn01@gmail.com>
2026-06-24 20:25:29 +0300

    fix: resolve duplicate header issue and fix icon syntax in TabLayout

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -2,23 +2,71 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import Colors from '../../../constants/colors'
-
+import {Feather} from "@expo/vector-icons"
+import { Octicons } from '@expo/vector-icons'
 const TabLayout = () => {
   return (
     <Tabs
     screenOptions={{
         tabBarInactiveTintColor:Colors.gray,
-        tabBarActiveTintColor:Colors.primary
+        tabBarActiveTintColor:Colors.primary,
+        tabBarStyle:{
+            backgroundColor:Colors.background,
+            borderColor:Colors.background
+        },
     }}
      >
-     <Tabs.Screen name="(home)/index"/>
-     <Tabs.Screen name="movies"/>
-     <Tabs.Screen name="series"/>
-     <Tabs.Screen name="profile"/>
-    </Tabs>
-  )
-}
+     <Tabs.Screen
+     name="(home)/index" 
+     options={{
+        tabBarIcon:({color}) =>
+             (
+         <Feather name="home" size={20} color={color}/>
+                  ),
+     tabBarLabel:"Home",
+    headerShown:false
+     }}/>
+      <Tabs.Screen
+      name="movies"
+       options={{
+        tabBarIcon:({color}) =>
+             (
+         <Feather name="play-circle" size={20} color={color}/>
+           ),
+          tabBarLabel:"Movies",
+        headerShown:false
+     }}/>
+      <Tabs.Screen 
+     name="series" 
+     options={{
+        tabBarIcon:({color}) => 
+            (
+         <Feather name="folder" size={20} color={color}/>
+            ),
+               tabBarLabel:"Series",
+        headerShown:false
+     }}/>
+     <Tabs.Screen 
+     name="profile"
+      options={{
+        tabBarIcon:({color}) => 
+            (
+         <Octicons name="person" size={20} color={color}/>
+            ),
+        tabBarLabel:"Me",
+        headerShown:false
+     }}/>
+    </Tabs>      
+    )
+}       
 
 export default TabLayout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({})   
+        
+     
+     
+   
+   
+
+
