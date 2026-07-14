7009881a98fca33bd863ec458015e8f5d0ebb653
esmagurgen <esmagrgn01@gmail.com>
2026-07-14 10:48:18 +0300

    feat: add types.d.ts and resolve typescript type issues in HomeScreen

--- src/app/(tabs)/(home)/index.tsx
+++ src/app/(tabs)/(home)/index.tsx
@@ -21,13 +21,23 @@ const SectionHeader = ({title}:{title:string})=>{
   
 
 const HomeScreen = () => {
-  
-  const {data,error,loading} = useFetch("/discover/movie",{
+  const currentYear=new Date().getFullYear();
+   const params={
     include_adult:false,
     include_video:false,
     language:"en-US",
     page:1,
     sort_by:"popularity.desc"
+  };
+  const {data:trendingData,loading:trendingLoading} = useFetch("/discover/movie",params)
+  const {data:newReleasesData,loading:newReleasesLoading} = useFetch("/discover/movie",{
+    ...params,
+    primary_release_year:currentYear,
+  })
+  const {data:internationalPickData,loading:internationalPickLoading} = useFetch("/discover/tv",params)
+  const {data:continueWatchingData,loading:continueWatchingLoading} = useFetch("/discover/tv",{
+    ...params,
+    page:2,
   })
   return (
      <ScrollView style={styles.container}  showsVerticalScrollIndicator={false}>
@@ -44,7 +54,7 @@ const HomeScreen = () => {
         <View style={styles.cardContainer}>
         <MovieCard genre={item.genre} title={item.title} image={item.image}/>
         </View>
-      )}
+      )}  
       
     />
      </View>
@@ -77,10 +87,27 @@ const HomeScreen = () => {
       )}
        />
      </View>
-    </View>   
+    </View> 
+     <View>
+        <SectionHeader title='International Picks'/>
+         <View style={styles.sectionHeader}>
+      <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={false}
+     contentContainerStyle={{paddingHorizontal:12, paddingBottom:10}}
+      data={movies}
+      keyExtractor={(item,index) => index.toString()}
+      renderItem={({item}) =>(
+        <View style={styles.cardContainer}>
+        <MovieCard genre={item.genre} title={item.title} image={item.image}/>
+        </View>
+      )}
+       />
+     </View>
+    </View>     
      </ScrollView>
-  )     
-} 
+  );     
+}
 
  
     
---
+++ types.d.ts
@@ -0,0 +1,17 @@
+export type Movie={
+adult:boolean;
+backdrop_path:string;
+genre_ids:number[];
+id:number;
+original_language:string;
+original_title:string;
+original_name:string;
+overview:string;
+popularity:Float;
+poster_path:string;
+release_date:string;
+title:string;
+video:boolean;
+vote_average:Float;
+vote__count:number;
+}
